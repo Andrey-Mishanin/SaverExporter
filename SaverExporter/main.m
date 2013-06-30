2306186f3d791b24849215967e2895310e4a63e2
Andrey Mishanin <andrey.mishanin@me.com>
2013-06-30 16:28:55 +0400

    Initial Commit

---
+++ SaverExporter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SaverExporter
+//
+//  Created by Andrey Mishanin on 30/06/2013.
+//  Copyright (c) 2013 Andrey Mishanin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
